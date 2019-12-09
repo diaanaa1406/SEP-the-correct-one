e29079e5a3f5ee97b96a93e528b5f9d2ecbfb28b
ajurj <ajurj12@yahoo.com>
2019-12-09 13:05:29 +0100

    mydate

--- Project/src/Clock.java
+++ Project/src/Clock.java
@@ -1,8 +1,64 @@
+import java.util.InputMismatchException;
+
 public class Clock
 {
+  private int hour;
+  private int minute;
+  public Clock(int hour, int minute)
+  {
+    try
+    {
+      this.hour = hour;
+    }catch (InputMismatchException e)
+    {
+      System.out.println("Incorrect input");
+    }
+    try
+    {
+      this.minute=minute;
+    }catch (InputMismatchException e)
+    {
+      System.out.println("Incorrect input");
+    }
+  }
+  public void setHour(int hour)
+  {
+    try
+    {
+      this.hour = hour;
+    }catch (InputMismatchException e)
+    {
+      System.out.println("Incorrect input");
+    }
 
+  }
 
+  public void setMinute(int minute)
+  {
+    try
+    {
+      this.minute=minute;
+    }catch (InputMismatchException e)
+    {
+      System.out.println("Incorrect input");
+    }
+  }
 
+  public int getHour()
+  {
+    return hour;
+  }
 
+  public int getMinute()
+  {
+    return minute;
+  }
 
+  public boolean equals(Object obj)
+  {
+    if(!(obj instanceof Clock))
+      return false;
+    Clock other=(Clock)obj;
+    return hour==other.hour && minute==other.minute;
+  }
 }
