96891407c2d0c8a1c40f9eb76494104b499efd0c
ajurj <ajurj12@yahoo.com>
2019-12-09 12:54:23 +0100

    mydate

--- Project/src/MyDate.java
+++ Project/src/MyDate.java
@@ -1,3 +1,72 @@
+import java.util.InputMismatchException;
+
 public class MyDate
 {
+  private int day;
+  private int month;
+  private int year;
+  public MyDate(int day,int year,int month)
+  {
+    try
+    {
+      this.day = day;
+    }catch (InputMismatchException e)
+    {
+      System.out.println("Incorrect input");
+    }
+    try
+    {
+      this.month=month;
+    }catch (InputMismatchException e)
+    {
+      System.out.println("Incorrect input");
+    }
+    try{
+      this.year=year;
+    }catch (InputMismatchException e)
+    {
+      System.out.println("Incorrect input");
+    }
+  }
+  public void setDay(int day)
+  {
+    try{
+      this.day=day;
+    }
+    catch (InputMismatchException e){
+      System.out.println("Incorrect input");
+    }
+  }
+  public void setMonth(int month)
+  {
+    try{
+      this.month=month;
+    }
+    catch (InputMismatchException e){
+      System.out.println("Incorrect input");
+    }
+  }
+  public void setYear(int year)
+  {
+    try{
+      this.year=year;
+    }
+    catch (InputMismatchException e){
+      System.out.println("Incorrect input");
+    }
+  }
+  public int getDay()
+  {
+    return day;
+  }
+
+  public int getMonth()
+  {
+    return month;
+  }
+
+  public int getYear()
+  {
+    return year;
+  }
 }
