78bd5d9a3eae324c9f6c36b0905e06e076cb1b3c
diaanaa1406 <dbocaniciu@gmail.com>
2019-12-09 13:08:04 +0100

    pls work

--- Project/src/ExternalPort.java
+++ Project/src/ExternalPort.java
@@ -1,3 +1,61 @@
+import java.util.InputMismatchException;
 public class ExternalPort
 {
+  private String type;
+  private int number;
+  public ExternalPort(String type, int number)
+  {
+    try
+    {
+      this.type=type;
+
+    }catch (InputMismatchException e)
+    {
+      System.out.println("Incorrect input");
+    }
+    try
+    {
+      this.number=number;
+    }catch (InputMismatchException e)
+    {
+      System.out.println("Incorrect input");
+    }
+
+  }
+  public void setType(String type)
+  {
+    try
+    {
+      this.type=type;
+
+    }catch (InputMismatchException e)
+    {
+      System.out.println("Incorrect input");
+    }  }
+  public void setNumber(int number)
+  {
+    try
+    {
+      this.number=number;
+    }catch (InputMismatchException e)
+    {
+      System.out.println("Incorrect input");
+    }
+  }
+  public String getType()
+  {
+    return type;
+  }
+  public int getNumber()
+  {
+    return number;
+  }
+
+  public boolean equals(Object obj)
+  {
+    if(!(obj instanceof ExternalPort))
+      return false;
+    ExternalPort other=(ExternalPort)obj;
+    return type.equals(other.type) && number==other.number;
+  }
 }
