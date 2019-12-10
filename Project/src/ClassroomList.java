719524dfaa7bf232989b0d2e2bacdd1b601931cd
diaanaa1406 <dbocaniciu@gmail.com>
2019-12-10 11:31:23 +0100

    pls work

--- Project/src/ClassroomList.java
+++ Project/src/ClassroomList.java
@@ -1,3 +1,29 @@
+import java.util.ArrayList;
 public class ClassroomList
 {
+  private ArrayList<Classroom> classrooms;
+
+
+  public ClassroomList()
+ {
+   classrooms=new ArrayList<Classroom>();
+
+ }
+  public void addClassroom(Classroom classroom)
+  {
+    classrooms.add(classroom);
+  }
+  public void removeClassroom(Classroom classroom)
+  {
+    classrooms.remove(classroom);
+  }
+  public Classroom getClassroom(int index)
+  {
+    return classrooms.get(index);
+  }
+  public String[] getAllClassrooms()
+  {
+    String[] classes=new String[classrooms.size()];
+    return classrooms.toArray(classes);
+  }
 }
