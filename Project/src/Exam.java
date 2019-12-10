dafd55a4b9f1daffea4c602c9d3bdecc08ff5a99
javier <javier.uw@gmail.com>
2019-12-10 11:22:21 +0100

    Hey! :p

--- Project/src/Exam.java
+++ Project/src/Exam.java
@@ -7,10 +7,46 @@ private MyDate date;
 private Clock startTime;
 private Clock endTime;
 private Classroom classroom;
+private CourseList courseList;
 private Course course;
-private ArrayList<Teacher> teachers;
+private TeacherList teacherList;
 
 public Exam(){}
+
+  public String getType()
+  {
+    return type;
+  }
+
+  public MyDate getDate()
+  {
+    return date;
+  }
+
+  public Clock getStartTime()
+  {
+    return startTime;
+  }
+
+  public Clock getEndTime()
+  {
+    return endTime;
+  }
+
+  public Classroom getClassroom()
+  {
+    return classroom;
+  }
+
+  public Course getCourse()
+  {
+    return course;
+  }
+
+  public TeacherList getTeacherList()
+  {
+    return teacherList;
+  }
 
   public void setType(String type){
     try{this.type = type;}
@@ -30,40 +66,21 @@ public Exam(){}
   public void setCourse(Course course){
     try{this.course = course;}
     catch(InputMismatchException e){System.out.println("incorrect input");} }
+  public void setTeacherList(TeacherList tl){
+    try{teacherList = tl;}
+    catch(InputMismatchException e){System.out.println("incorrect input");} }
 
-    public int getNumberOfTeachers(){return teachers.size();}
 
-    public void addTeacher(Teacher teacher){
-    try
-    { teachers.add(teacher); }
-    catch (InputMismatchException e)
-    { System.out.println("That is not an exam"); }
-    }
-
-  public void removeTeacher(Teacher teacher){
-    for(int i=0;i<teachers.size();i++){
-      if (teachers.get(i).equals(teacher)){teachers.remove(i);};
-    }
-  }
-
-  public String getAllTeachers(){
-    String Str = "";
-    for (int i=0;i<teachers.size();i++)
-    {Str += teachers.get(i);}
-    return Str;
-  }
 
   public boolean equals(Object obj){
   if(!(obj instanceof Exam)){return false;}
   Exam other = (Exam)obj;
-  boolean sameteachers = true;
-  for (int i=0; i<teachers.size();i++){if(!(teachers.get(i).equals(other.teachers.get(i)))){sameteachers = false;}}
   return other.type.equals(type) && other.course.equals(course) && other.classroom.equals(classroom) &&
-      other.date.equals(date) && other.startTime.equals(startTime) && other.endTime.equals(endTime) && sameteachers;
+      other.date.equals(date) && other.startTime.equals(startTime) && other.endTime.equals(endTime) && other.teacherList.equals(teacherList);
   }
 
   public String toString(){
-  return type+date+startTime+endTime+classroom+course+getAllTeachers();
+  return type+date+startTime+endTime+classroom+course+teacherList;
   }
 }
 
--- Project/src/TeacherList.java
+++ Project/src/TeacherList.java
@@ -1,3 +1,39 @@
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+
 public class TeacherList
 {
+
+  private ArrayList<Teacher> teachers;
+
+  public int getNumberOfTeachers(){return teachers.size();}
+
+  public void addTeacher(Teacher teacher){
+    try
+    { teachers.add(teacher); }
+    catch (InputMismatchException e)
+    { System.out.println("That is not an exam"); }
+  }
+
+  public void removeTeacher(Teacher teacher){
+    for(int i=0;i<teachers.size();i++){
+      if (teachers.get(i).equals(teacher)){teachers.remove(i);};
+    }
+  }
+
+  public String getAllTeachers(){
+    String Str = "";
+    for (int i=0;i<teachers.size();i++)
+    {Str += teachers.get(i);}
+    return Str;
+  }
+
+  public boolean equals(Object obj) {
+    if (!(obj instanceof TeacherList)){return false;}
+    TeacherList other = (TeacherList)obj;
+    boolean sameteachers = true;
+    for (int i=0; i<teachers.size();i++){if(!(teachers.get(i).equals(other.teachers.get(i)))){sameteachers = false;}}
+    return sameteachers;
+  }
+
 }
